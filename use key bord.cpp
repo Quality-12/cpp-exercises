33c4c70cdcc0669c08912e9e92e801df1e18a9fa
Quality-12 <yealemt81@gmail.com>
2025-07-19 08:24:30 -0300

    Add files via upload

---
+++ use key bord.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+using namespace std;
+int main(){
+int age ;
+cout<<"enter age";
+	cin >>age;
+if (age >7){ 	
+cout <<"you can use a computey";}
+else{
+
+cout<<"don't use";
+}
+
+}
+
+
+
+
+	
+	
