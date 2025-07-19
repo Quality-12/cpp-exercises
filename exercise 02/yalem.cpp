cb120792703bd314460942d0f12422ad65764ded
Quality-12 <yealemt81@gmail.com>
2025-07-19 08:03:11 -0300

    Add files via upload

---
+++ exercise 02/yalem.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main(){
+int age =10;
+float height=1.33;
+string name="yalem";
+cout<<name<< " is "<< age <<"yearsold and "<<height<<"m tall.";
+}
+	
+	
