46873355be01928206b626de652fd4f52ae5adbe
Quality-12 <yealemt81@gmail.com>
2025-07-22 02:39:57 -0700

    Add files via upload

---
+++ f struct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct student{
+	string name;
+	int age;
+};
+int main(){
+
+
+
+student s1;
+s1.name="selam";
+s1.age =11;
+cout<<s1.name<<"is"	<<s1.age<<"years old.";
+	
+
+
+
+return 0;
+}
+
