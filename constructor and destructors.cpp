be52bb541509aaff41e65c4b2b777b9bf28193f3
Quality-12 <yealemt81@gmail.com>
2025-07-22 03:48:15 -0700

    Add files via upload

---
+++ constructor and destructors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class book{
+public:
+book(){
+cout<<"book is opened"<<endl;
+}
+~book()	{
+	cout<<"book is closed";
+}	
+};
+	
+int main(){
+	
+	book b;
+	return 0;
+}		
+	
+
+
+
+
+
+
+
+
+
+	
+	
+
